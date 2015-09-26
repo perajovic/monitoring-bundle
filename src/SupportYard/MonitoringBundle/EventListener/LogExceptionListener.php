7ca92cb1131d66842e0fe3b76c3be96fc37b425f
Pera Jovic <pera@codecontrol.rs>
2015-09-26 18:29:28 +0200

    Added LogExceptionListener

---
+++ src/SupportYard/MonitoringBundle/EventListener/LogExceptionListener.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SupportYard\MonitoringBundle\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\FlattenException;
+use Symfony\Component\Templating\EngineInterface;
+
+class LogExceptionListener
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var EngineInterface
+     */
+    private $templating;
+
+    /**
+     * @param LoggerInterface $logger
+     * @param EngineInterface $templating
+     */
+    public function __construct(LoggerInterface $logger, EngineInterface $templating)
+    {
+        $this->logger = $logger;
+        $this->templating = $templating;
+    }
+
+    /**
+     * @param GetResponseForExceptionEvent $event
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = FlattenException::create($event->getException());
+
+        $content = $this->templating->render(
+            'MonitoringBundle:traceForLog.txt.twig',
+            ['exception' => $exception]
+        );
+
+        $this->logger->info(htmlspecialchars_decode($content, ENT_QUOTES));
+    }
+}
---
+++ src/SupportYard/MonitoringBundle/Tests/Unit/EventListener/LogExceptionListenerTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace SupportYard\MonitoringBundle\Tests\Unit\EventListener;
+
+use Codecontrol\FrameworkBundle\Test\EventListenerTestCase;
+use Exception;
+use SupportYard\MonitoringBundle\EventListener\LogExceptionListener;
+
+class LogExceptionListenerTest extends EventListenerTestCase
+{
+    /**
+     * @test
+     */
+    public function exceptionIsLogged()
+    {
+        $content = 'Foo template';
+        $exception = new Exception('Foo');
+
+        $this->ensureException($exception);
+        $this->ensureTemplateContent($content);
+        $this->ensureDataIsLogged($content);
+
+        $this->listener->onKernelException($this->event);
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->event = $this->createGetResponseForExceptionEvent();
+        $this->logger = $this->createLogger();
+        $this->templating = $this->createTemplating();
+        $this->listener = new LogExceptionListener($this->logger, $this->templating);
+    }
+
+    private function ensureDataIsLogged($content)
+    {
+        $this
+            ->logger
+            ->expects($this->once())
+            ->method('info')
+            ->with($content);
+    }
+
+    private function ensureTemplateContent($content)
+    {
+        $this
+            ->templating
+            ->expects($this->once())
+            ->method('render')
+            ->with(
+                'MonitoringBundle:traceForLog.txt.twig',
+                $this->callback(function ($data) {
+                    $this->assertInstanceOf(
+                        'Symfony\Component\HttpKernel\Exception\FlattenException',
+                        $data['exception']
+                    );
+
+                    return $data;
+                })
+            )
+            ->will($this->returnValue($content));
+    }
+
+    private function ensureException($exception)
+    {
+        $this
+            ->event
+            ->expects($this->once())
+            ->method('getException')
+            ->will($this->returnValue($exception));
+    }
+
+    private function createLogger()
+    {
+        return $this->createMockFor('Psr\Log\LoggerInterface');
+    }
+
+    private function createTemplating()
+    {
+        return $this->createMockFor('Symfony\Component\Templating\EngineInterface');
+    }
+}
